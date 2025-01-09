5d7dba4509f6e7d26f66dfab9d8c08491d78a78e
Eric Funk <psytraxx@users.noreply.github.com>
2025-01-09 21:05:47 +0100

    mipidsi master (#6)
    * mipidsi master
    
    * Refactor RM67162 display driver
    
    * Refactor display error handling to use a custom DisplayError type
    
    * Update dependencies and refactor touchpad handling to use embedded-drivers-rs
    
    * fix  touchpad handling

--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,9 @@
-use crate::dma::SPIInterface;
-use crate::rm67162::RM67162;
-
 use core::convert::Infallible;
 use defmt::info;
-use display_interface::DisplayError;
+use embedded_drivers_rs::mipidsi::interface::{SpiError, SpiInterface};
+use embedded_drivers_rs::mipidsi::options::{Orientation, Rotation};
+use embedded_drivers_rs::mipidsi::{Builder, Display as MipiDisplay};
+use embedded_drivers_rs::rm67162::RM67162;
 use embedded_graphics::draw_target::DrawTarget;
 use embedded_graphics::geometry::Point;
 use embedded_graphics::mono_font::iso_8859_1::FONT_10X20 as FONT;
@@ -14,15 +14,14 @@ use embedded_graphics::primitives::{Line, PrimitiveStyle};
 use embedded_graphics::text::{Baseline, Text};
 use embedded_graphics::Drawable;
 use embedded_graphics_framebuf::FrameBuf;
+use embedded_hal_bus::spi::{DeviceError, ExclusiveDevice};
 use esp_hal::delay::Delay;
-use esp_hal::dma::Dma;
+use esp_hal::dma::{Dma, DmaRxBuf, DmaTxBuf};
 use esp_hal::gpio::{GpioPin, Level, Output};
 use esp_hal::peripherals::{DMA, SPI2};
-use esp_hal::prelude::*;
-use esp_hal::spi::master::{Config, Spi};
-use mipidsi::error::InitError;
-use mipidsi::options::Orientation;
-use mipidsi::{Builder, Display as MipiDisplay};
+use esp_hal::spi::master::{Config, Spi, SpiDmaBus};
+use esp_hal::spi::Error;
+use esp_hal::{dma_buffers, prelude::*};
 
 use crate::config::{DISPLAY_HEIGHT, DISPLAY_WIDTH};
 
@@ -30,7 +29,19 @@ const TEXT_STYLE: MonoTextStyle<Rgb565> = MonoTextStyle::new(&FONT, Rgb565::WHIT
 pub const LCD_PIXELS: usize = (DISPLAY_HEIGHT as usize) * (DISPLAY_WIDTH as usize);
 type DisplayBuffer = [Rgb565; LCD_PIXELS];
 
-pub type MipiDisplayWrapper<'a> = MipiDisplay<SPIInterface<'a>, RM67162, Output<'a>>;
+pub type MipiDisplayWrapper<'a> = MipiDisplay<
+    SpiInterface<
+        'a,
+        ExclusiveDevice<
+            SpiDmaBus<'a, esp_hal::Blocking>,
+            Output<'a>,
+            embedded_hal_bus::spi::NoDelay,
+        >,
+        Output<'a>,
+    >,
+    RM67162,
+    Output<'a>,
+>;
 
 pub struct Display<'a> {
     display: MipiDisplayWrapper<'a>,
@@ -42,6 +53,9 @@ pub struct Display<'a> {
 /// Provides basic drawing operations for text and primitives.
 /// Implementations should handle the low-level display communication.
 pub trait DisplayTrait {
+    /// Error type
+    type Error: core::fmt::Debug;
+
     /// Writes text to the display at the specified position
     ///
     /// # Arguments
@@ -51,14 +65,14 @@ pub trait DisplayTrait {
     /// # Returns
     /// * `Ok(())` on successful write
     /// * `Err(Error)` if the write operation fails
-    fn write(&mut self, text: &str, position: Point) -> Result<(), Error>;
+    fn write(&mut self, text: &str, position: Point) -> Result<(), Self::Error>;
 
     /// Updates the display with the current framebuffer contents
     ///
     /// # Returns
     /// * `Ok(())` on successful update
     /// * `Err(Error)` if the update operation fails
-    fn update_with_buffer(&mut self) -> Result<(), Error>;
+    fn update_with_buffer(&mut self) -> Result<(), Self::Error>;
 
     /// Draws a line between two points
     ///
@@ -69,7 +83,7 @@ pub trait DisplayTrait {
     /// # Returns
     /// * `Ok(())` on successful line draw
     /// * `Err(Error)` if the draw operation fails
-    fn draw_line(&mut self, begin: Point, end: Point) -> Result<(), Error>;
+    fn draw_line(&mut self, begin: Point, end: Point) -> Result<(), Self::Error>;
 }
 
 pub struct DisplayPeripherals {
@@ -84,7 +98,7 @@ pub struct DisplayPeripherals {
 }
 
 impl<'a> Display<'a> {
-    pub fn new(p: DisplayPeripherals) -> Result<Self, Error> {
+    pub fn new(p: DisplayPeripherals, buffer: &'a mut [u8]) -> Result<Self, DisplayError> {
         // SPI pins
         let sck = Output::new(p.sck, Level::Low);
         let mosi = Output::new(p.mosi, Level::Low);
@@ -97,7 +111,7 @@ impl<'a> Display<'a> {
         let dma = Dma::new(p.dma);
 
         // Configure SPI
-        let spi_bus = Spi::new_with_config(
+        let spi = Spi::new_with_config(
             p.spi,
             Config {
                 frequency: 75.MHz(),
@@ -113,17 +127,23 @@ impl<'a> Display<'a> {
 
         let dc_pin = p.dc;
         let rst_pin = p.rst;
+        let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(32000);
+        let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+        let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
 
-        let di = SPIInterface::new(LCD_PIXELS, spi_bus, Output::new(dc_pin, Level::Low), cs);
+        let spi = SpiDmaBus::new(spi, dma_rx_buf, dma_tx_buf);
+
+        let spi_device = ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+
+        let di = SpiInterface::new(spi_device, Output::new(dc_pin, Level::Low), buffer);
 
         let mut delay = Delay::new();
 
         let display = Builder::new(RM67162, di)
             .orientation(Orientation {
                 mirrored: false,
-                rotation: mipidsi::options::Rotation::Deg90,
+                rotation: Rotation::Deg270,
             })
-            .display_size(DISPLAY_WIDTH, DISPLAY_HEIGHT)
             .reset_pin(Output::new(rst_pin, Level::High))
             .init(&mut delay)
             .unwrap();
@@ -137,19 +157,21 @@ impl<'a> Display<'a> {
 }
 
 impl<'a> DisplayTrait for Display<'a> {
-    fn write(&mut self, text: &str, position: Point) -> Result<(), Error> {
+    type Error = DisplayError;
+
+    fn write(&mut self, text: &str, position: Point) -> Result<(), Self::Error> {
         Text::with_baseline(text, position, TEXT_STYLE, Baseline::Top).draw(&mut self.framebuf)?;
         Ok(())
     }
 
-    fn draw_line(&mut self, start: Point, end: Point) -> Result<(), Error> {
+    fn draw_line(&mut self, start: Point, end: Point) -> Result<(), Self::Error> {
         Line::new(start, end)
             .into_styled(PrimitiveStyle::with_stroke(RgbColor::WHITE, 2))
             .draw(&mut self.framebuf)?;
         Ok(())
     }
 
-    fn update_with_buffer(&mut self) -> Result<(), Error> {
+    fn update_with_buffer(&mut self) -> Result<(), Self::Error> {
         let pixel_iterator = self.framebuf.into_iter().map(|p| p.1);
 
         self.display
@@ -161,25 +183,19 @@ impl<'a> DisplayTrait for Display<'a> {
     }
 }
 
-/// A clock error
 #[derive(Debug)]
-pub enum Error {
-    DisplayInterface(#[expect(unused, reason = "Never read directly")] DisplayError),
+pub enum DisplayError {
     Infallible,
+    SpiError(#[allow(unused)] SpiError<DeviceError<Error, Infallible>, Infallible>),
 }
 
-impl From<DisplayError> for Error {
-    fn from(error: DisplayError) -> Self {
-        Self::DisplayInterface(error)
+impl From<SpiError<DeviceError<Error, Infallible>, Infallible>> for DisplayError {
+    fn from(err: SpiError<DeviceError<Error, Infallible>, Infallible>) -> Self {
+        DisplayError::SpiError(err)
     }
 }
 
-impl From<InitError<Infallible>> for Error {
-    fn from(_: InitError<Infallible>) -> Self {
-        Self::Infallible
-    }
-}
-impl From<Infallible> for Error {
+impl From<Infallible> for DisplayError {
     fn from(_: Infallible) -> Self {
         Self::Infallible
     }
